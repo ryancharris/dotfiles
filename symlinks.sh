cbe65b83fd448ca9cbddad2141fbdbc225ec782d
Ryan Harris <harrisryan1@gmail.com>
2020-03-16 12:14:05 -0400

    Move vscode settings.json into dotfiles

--- symlinks.sh
+++ symlinks.sh
@@ -1,2 +1,3 @@
 ln -s dotfiles/github/.gitconfig ~/.gitconfig
+ln -s dotfiles/vscode/settings.json /Users/ryan/Library/Application\ Support/Code/User/settings.json
 ln -s dotfiles/zsh/.zshrc ~/.zshrc
