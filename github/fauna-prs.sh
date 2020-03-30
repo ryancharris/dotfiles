3743875a204feabad087af0612028ca49fc3d507
Ryan Harris <harrisryan1@gmail.com>
2020-03-30 11:01:26 -0400

    Add loop to fauna-prs.sh

--- github/fauna-prs.sh
+++ github/fauna-prs.sh
@@ -1,8 +1,8 @@
-cd ~/Code/console2
-gh pr status
-cd ~/Code/fauna-shell
-gh pr status
-cd ~/Code/faunadb-js
-gh pr status
-cd ~/Code/vscode
-gh pr status
+function get_pr_status() {
+  gh pr status
+}
+
+for dir in ~/Fauna/*/; do
+    cd $dir
+    get_pr_status
+done
