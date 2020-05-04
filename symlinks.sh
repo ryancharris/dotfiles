693cdda14195848afa0ea847fb476b802ca1e5f6
Ryan Harris <harrisryan1@gmail.com>
2020-05-04 17:28:34 -0400

    remove vscode config from symlinks.sh

--- symlinks.sh
+++ symlinks.sh
@@ -1,3 +1,2 @@
 ln -s dotfiles/github/.gitconfig ~/.gitconfig
-ln -s dotfiles/vscode/settings.json /Users/ryan/Library/Application\ Support/Code/User/settings.json
 ln -s dotfiles/zsh/.zshrc ~/.zshrc
