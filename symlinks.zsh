c9644cb78656bd0d22bd43950a9df46a37dc6e55
Ryan Harris <harrisryan1@gmail.com>
2020-08-07 19:30:42 -0400

    rename doom directory, add symlink

--- symlinks.zsh
+++ symlinks.zsh
@@ -2,3 +2,4 @@ ln -s dotfiles/github/.gitconfig ~/.gitconfig
 ln -s dotfiles/zsh/.zshrc ~/.zshrc
 ln -s dotfiles/.tmux.conf ~/.tmux.conf
 ln -s dotfiles/.alacritty.yml ~/.alacritty.yml
+ln -s dotfiles/.doom.d ~/.doom.d
