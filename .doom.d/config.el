ae8b9bfdba88c999ba30063a4a61c769217994fd
Ryan Harris <harrisryan1@gmail.com>
2020-08-13 14:46:04 -0400

    remove doom splash screen

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,7 +3,6 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Ryan Harris"
