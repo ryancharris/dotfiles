c06ec6c90ef3faf6504b22e89d5945f0d64bd5f9
Ryan Harris <harrisryan1@gmail.com>
2020-08-17 09:12:56 -0400

    add org-roam

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -89,3 +89,18 @@
 
 (setq indent-tabs-mode t)
 (setq standard-indent 2)
+
+;; Configure org-roam
+(use-package org-roam
+      :ensure t
+      :hook
+      (after-init . org-roam-mode)
+      :custom
+      (org-roam-directory "~/org/")
+      :bind (:map org-roam-mode-map
+              (("C-c n l" . org-roam)
+               ("C-c n f" . org-roam-find-file)
+               ("C-c n g" . org-roam-graph-show))
+              :map org-mode-map
+              (("C-c n i" . org-roam-insert))
+              (("C-c n I" . org-roam-insert-immediate))))
