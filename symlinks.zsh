5cfc89cfb899cca2e3a73c9b6b04a3276b222e2f
Ryan Harris <harrisryan1@gmail.com>
2021-01-10 14:13:19 -0500

    fix tmux

--- symlinks.zsh
+++ symlinks.zsh
@@ -1,5 +1,6 @@
 ln -s dotfiles/github/.gitconfig ~/.gitconfig
 ln -s dotfiles/zsh/.zshrc ~/.zshrc
-ln -s dotfiles/.tmux.conf ~/.tmux.conf
+ln -s dotfiles/.tmux/.tmux.conf ~/.tmux.conf
+ln -s dotfiles/.tmux/.tmux.conf.local ~/.tmux.conf.local
 ln -s dotfiles/.alacritty.yml ~/.alacritty.yml
 ln -s dotfiles/.doom.d ~/.doom.d
