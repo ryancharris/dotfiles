bf8eef7e44e009402d459a1e3db540760ecd23c5
ryan <ryan@brewtown.local>
2021-01-27 13:25:38 -0500

    add git dir, update symlinks

--- symlinks.zsh
+++ symlinks.zsh
@@ -1,6 +1,16 @@
-ln -s dotfiles/.gitconfig ~/.gitconfig
-ln -s dotfiles/zsh/.zshrc ~/.zshrc
+# git
+ln -s dotfiles/git/.gitconfig ~/.gitconfig
+ls -s dotfiles/git/.gitignore ~/.gitignore
+
+# tmux
 ln -s dotfiles/.tmux/.tmux.conf ~/.tmux.conf
 ln -s dotfiles/.tmux/.tmux.conf.local ~/.tmux.conf.local
+
+# alacritty
 ln -s dotfiles/.alacritty.yml ~/.alacritty.yml
+
+# zsh
+ln -s dotfiles/zsh/.zshrc ~/.zshrc
+
+# emacs
 ln -s dotfiles/.doom.d ~/.doom.d
