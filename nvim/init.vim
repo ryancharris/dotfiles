da955706806f72b80dedbb4de6bb1e36d379b78c
Ryan Harris <harrisryan1@gmail.com>
2023-02-10 21:13:22 -0500

    use packer instead of plug

--- nvim/init.vim
+++ nvim/init.vim
@@ -1,3 +1,8 @@
+lua require('plugins')
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 source ~/.vimrc
+augroup packer_user_config
+  autocmd!
+  autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+augroup end
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,35 @@
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+vim.cmd [[colorscheme tokyonight-moon]]
+
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+    use "nvim-lua/plenary.nvim"
+    use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.1',
+      requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use 'folke/tokyonight.nvim'
+    use {
+      "ahmedkhalf/project.nvim",
+      config = function()
+          require('telescope').load_extension('projects')
+          require'telescope'.extensions.projects.projects{}
+
+        require("project_nvim").setup {
+        }
+      end
+    }
+    use {
+            "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
+end)
