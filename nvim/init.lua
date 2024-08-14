22855c0051052528ff8569ac9ef03693e29713c9
Ryan Harris <harrisryan1@gmail.com>
2024-08-14 19:03:50 -0400

    update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,6 +12,7 @@ require("bufferline").setup{}
 require('lualine').setup()
 
 vim.cmd([[
+let mapleader = " "
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 filetype on
@@ -34,5 +35,20 @@ set softtabstop=4
 -- map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
 -- map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
 
-vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", { noremap = true, silent = true })
+-- editing
+vim.api.nvim_set_keymap("i", "<leader><Tab>", "<cmd>lua vim.lsp.omnifunc()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>rr", "<cmd>redo<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>uu", "<cmd>undo<CR>", { noremap = true, silent = true })
+
+-- lsp
+vim.api.nvim_set_keymap("n", "<leader>cd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>cr", "<cmd>lua vim.lsp.buf.references()<CR>", { noremap = true, silent = true })
+
+
+-- windows
+vim.api.nvim_set_keymap("n", "<leader>wh", "<cmd>hsplit<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wv", "<cmd>vsplit<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wl", "<cmd>wincmd l<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wh", "<cmd>wincmd h<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wj", "<cmd>wincmd j<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wk", "<cmd>wincmd k<CR>", { noremap = true, silent = true })
