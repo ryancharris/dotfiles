c965829e2fa890b3429f81a8c03883b2eb31885e
Ryan Harris <harrisryan1@gmail.com>
2024-10-08 09:13:04 -0400

    update nvim search, keybindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -33,13 +33,11 @@ vim.api.nvim_set_keymap("n", "<leader>cr", "<cmd>lua vim.lsp.buf.references()<CR
 
 -- searching
 vim.o.grepprg="git grep -n"
-vim.api.nvim_create_user_command("SearchCurrentWord", '<cmd>silent grep! <cword> | copen', {})
 vim.api.nvim_create_user_command("SearchWithPrompt", 'silent grep! <args> | copen', { nargs = '+'})
-vim.api.nvim_set_keymap("n", "<leader>ps", ":SearchCurrentWord<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ps", "<cmd>silent grep! <cword> | copen <CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>ss", ":SearchWithPrompt", { noremap = true, silent = true })
 
 -- windows
-vim.api.nvim_set_keymap("n", "<leader>wh", "<cmd>hsplit<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>wv", "<cmd>vsplit<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>wl", "<cmd>wincmd l<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>wh", "<cmd>wincmd h<CR>", { noremap = true, silent = true })
