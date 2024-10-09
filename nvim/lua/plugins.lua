ef7e63bc421ea5488de4303bae202afeececb5d3
Ryan Harris <harrisryan1@gmail.com>
2024-10-09 12:59:20 -0400

    lualine icons working

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -24,14 +24,13 @@ return require('packer').startup(function(use)
             require('Comment').setup()
         end
     }
+    use 'nvim-tree/nvim-web-devicons'
     use {
       'nvim-lualine/lualine.nvim',
       requires = { 'kyazdani42/nvim-web-devicons', opt = true },
       config = function()
           require ("lualine").setup{
               sections = {
-                  lualine_a = {'mode'},
-                  lualine_b = {'branch', 'diff', 'diagnostics'},
                   lualine_c = {
                       {
                           'filename',
@@ -39,8 +38,6 @@ return require('packer').startup(function(use)
                       }
                   },
                   lualine_x = {'filetype'},
-                  lualine_y = {'progress'},
-                  lualine_z = {'location'}
               }
 
           }
