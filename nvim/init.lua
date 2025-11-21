f12628d1548ed7474e8650adbb81048036e0b58f
Ryan Harris <harrisryan1@gmail.com>
2025-11-21 11:46:54 -0500

    kitty: reduce config

--- nvim/init.lua
+++ nvim/init.lua
@@ -68,3 +68,14 @@ vim.lsp.enable('ts_ls', {
     }
 })
 vim.lsp.enable('yamlls')
+
+-- file specific
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {"javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx"},
+  callback = function()
+    vim.bo.shiftwidth = 2  -- Indent size for each level
+    vim.bo.softtabstop = 2 -- Spaces per Tab press
+    vim.bo.expandtab = true -- Use spaces instead of tabs
+  end,
+})
+
