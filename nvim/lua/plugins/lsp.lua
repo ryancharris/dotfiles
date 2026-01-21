c41aa1b98edfaac19db6d3f347a1b33f3d422988
Ryan Harris <harrisryan1@gmail.com>
2026-01-21 16:21:56 -0500

    nvim: configure lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -23,17 +23,26 @@ return {
                 ensure_installed = servers,
             })
 
-            local lspconfig = require("lspconfig")
-
-            for _, server in ipairs(servers) do
-                if server == "eslint" then
-                    lspconfig.eslint.setup({
-                        filetypes = { "javascript", "javascriptreact", "javascript.jsx", "vue", "svelte", "astro" },
-                    })
-                else
-                    lspconfig[server].setup({})
-                end
+            -- Setup completion capabilities if available
+            local capabilities = {}
+            local has_cmp, cmp_lsp = pcall(require, "cmp_nvim_lsp")
+            if has_cmp then
+                capabilities = cmp_lsp.default_capabilities()
             end
+
+            -- Set global defaults for all LSP servers
+            vim.lsp.config("*", { capabilities = capabilities })
+
+            -- Configure and enable servers using the new Neovim 0.11+ API
+            for _, server in ipairs(servers) do
+                local opts = {}
+                if server == "eslint" then
+                    opts.filetypes = { "javascript", "javascriptreact", "javascript.jsx", "vue", "svelte", "astro" }
+                end
+                vim.lsp.config(server, opts)
+            end
+
+            vim.lsp.enable(servers)
         end,
     },
 }
