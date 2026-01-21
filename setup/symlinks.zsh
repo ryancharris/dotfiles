514941b5f4cd5c8ee9cd3028172173f7b7b98551
Ryan Harris <harrisryan1@gmail.com>
2026-01-21 14:18:15 -0500

    fixup! ai: add agents sub-dir

--- setup/symlinks.zsh
+++ setup/symlinks.zsh
@@ -28,3 +28,7 @@ ln -s ~/dotfiles/k9s/skin.yml ~/Library/Application\ Support/k9s/skin.yml
 # kitty
 ln -s ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
 ln -s ~/dotfiles/kitty/current-theme.conf ~/.config/kitty/current-theme.conf
+
+# agents
+ln -s ~/dotfiles/agents/gemini/GEMINI.md ~/.gemini/GEMINI.md
+ln -s ~/dotfiles/agents/gemini/settings.json ~/.gemini/settings.json
