83d90a4205351a63adf91c916601f83283a55154
Ryan Harris <harrisryan1@gmail.com>
2026-01-21 14:20:21 -0500

    nvim: fix lsp config

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,48 +2,6 @@ require("config.options")
 require("config.lazy")
 require('config.keymaps')
 
--- lsps
-vim.lsp.enable('bashls')
-vim.lsp.enable('cssls')
-vim.lsp.enable('dockerls')
-vim.lsp.enable('eslint', {
-    filetypes = {"javascript", "javascriptreact", "javascript.jsx", "vue", "svelte", "astro"}
-})
-vim.lsp.enable('gopls')
-vim.lsp.enable('html')
-vim.lsp.enable('jsonls')
-vim.lsp.enable('pyright')
-vim.lsp.enable('rust_analyzer')
-vim.lsp.enable('terraformls')
-vim.lsp.enable('ts_ls', {
-    filetypes = {"typescript", "typescriptreact", "typescript.tsx"},
-    settings = {
-        typescript = {
-            inlayHints = {
-                includeInlayParameterNameHints = "all",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-            }
-        },
-        javascript = {
-            inlayHints = {
-                includeInlayParameterNameHints = "all",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-            }
-        }
-    }
-})
-vim.lsp.enable('yamlls')
-
 -- file specific
 vim.api.nvim_create_autocmd("FileType", {
   pattern = {"javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx"},
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,8 +1,69 @@
 return {
     {
-        "williamboman/mason.nvim", opts = {}
+        "williamboman/mason.nvim",
+        opts = {},
     },
     {
-        "neovim/nvim-lspconfig"
-    }
+        "williamboman/mason-lspconfig.nvim",
+        opts = {
+            ensure_installed = {
+                "bashls", "cssls", "dockerls", "eslint", "gopls",
+                "html", "jsonls", "pyright", "rust_analyzer",
+                "terraformls", "ts_ls", "yamlls"
+            },
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            local mason_lspconfig = require("mason-lspconfig")
+
+            -- Standard setup for all servers installed via Mason
+            mason_lspconfig.setup_handlers({
+                -- The default handler: setup with default settings
+                function(server_name)
+                    lspconfig[server_name].setup({})
+                end,
+
+                -- Custom handler for 'ts_ls' (TypeScript)
+                ["ts_ls"] = function()
+                    lspconfig.ts_ls.setup({
+                        filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+                        settings = {
+                            typescript = {
+                                inlayHints = {
+                                    includeInlayParameterNameHints = "all",
+                                    includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                                    includeInlayFunctionParameterTypeHints = true,
+                                    includeInlayVariableTypeHints = true,
+                                    includeInlayPropertyDeclarationTypeHints = true,
+                                    includeInlayFunctionLikeReturnTypeHints = true,
+                                    includeInlayEnumMemberValueHints = true,
+                                },
+                            },
+                            javascript = {
+                                inlayHints = {
+                                    includeInlayParameterNameHints = "all",
+                                    includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                                    includeInlayFunctionParameterTypeHints = true,
+                                    includeInlayVariableTypeHints = true,
+                                    includeInlayPropertyDeclarationTypeHints = true,
+                                    includeInlayFunctionLikeReturnTypeHints = true,
+                                    includeInlayEnumMemberValueHints = true,
+                                },
+                            },
+                        },
+                    })
+                end,
+
+                -- Custom handler for 'eslint'
+                ["eslint"] = function()
+                    lspconfig.eslint.setup({
+                        filetypes = { "javascript", "javascriptreact", "javascript.jsx", "vue", "svelte", "astro" },
+                    })
+                end,
+            })
+        end,
+    },
 }
