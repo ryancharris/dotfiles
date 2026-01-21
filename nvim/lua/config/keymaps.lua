af49805b633c94c9be914feaba10ff59b705db2a
Ryan Harris <harrisryan1@gmail.com>
2026-01-21 14:17:22 -0500

    nvim: update keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,47 +1,41 @@
 local functions = require("config.functions")
 
 -- buffers
-vim.api.nvim_set_keymap('n', '<leader><Tab>', ':b#<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>wd", "<cmd>bdelete<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>bb", "<cmd>FzfLua buffers<CR>", { noremap = true, silent = true })
+vim.keymap.set('n', '<leader><Tab>', ':b#<CR>', { silent = true })
+vim.keymap.set("n", "<leader>wd", "<cmd>bdelete<CR>", { silent = true })
+vim.keymap.set("n", "<leader>bb", "<cmd>FzfLua buffers<CR>", { silent = true })
 
 -- editing
-vim.api.nvim_set_keymap("n", "<leader>rr", "<cmd>redo<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>uu", "<cmd>undo<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>rr", "<cmd>redo<CR>", { silent = true })
+vim.keymap.set("n", "<leader>uu", "<cmd>undo<CR>", { silent = true })
 
 -- files
-vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>FzfLua files<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>ee", "<cmd>Explore<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>rr", "<cmd>Rexplore<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<CR>", { silent = true })
+vim.keymap.set("n", "<leader>ee", "<cmd>Explore<CR>", { silent = true })
+vim.keymap.set("n", "<leader>re", "<cmd>Rexplore<CR>", { silent = true })
 
 -- git
-vim.api.nvim_set_keymap("n", "<leader>mb", "<cmd>FzfLua git_blame<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>mb", "<cmd>FzfLua git_blame<CR>", { silent = true })
 
 -- github
 vim.api.nvim_create_user_command("OpenInGithub", functions.open_in_github, {})
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>gg",
-  ":OpenInGithub<CR>",
-  { desc = "Open in GitHub", noremap = true, silent = true }
-)
+vim.keymap.set("n", "<leader>gg", "<cmd>OpenInGithub<CR>", { desc = "Open in GitHub", silent = true })
 
 -- lsp
 vim.api.nvim_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-vim.api.nvim_set_keymap("n", "<leader>cd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>cr", "<cmd>FzfLua lsp_references<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>co", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { silent = true })
+vim.keymap.set("n", "<leader>cr", "<cmd>FzfLua lsp_references<CR>", { silent = true })
+vim.keymap.set("n", "<leader>co", vim.diagnostic.open_float, { silent = true })
 
 -- searching
-vim.o.grepprg="git grep -n"
-vim.api.nvim_set_keymap("n", "<leader>ps", "<cmd>FzfLua grep_cword<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>ss", "<cmd>FzfLua grep<CR>", { noremap = true })
+vim.o.grepprg = "git grep -n"
+vim.keymap.set("n", "<leader>ps", "<cmd>FzfLua grep_cword<CR>", { silent = true })
+vim.keymap.set("n", "<leader>ss", "<cmd>FzfLua grep<CR>")
 
 -- windows
-vim.api.nvim_set_keymap("n", "<leader>wv", "<cmd>vsplit<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>ws", "<cmd>split<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>wl", "<cmd>wincmd l<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>wh", "<cmd>wincmd h<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>wj", "<cmd>wincmd j<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>wk", "<cmd>wincmd k<CR>", { noremap = true, silent = true })
-
+vim.keymap.set("n", "<leader>wv", "<cmd>vsplit<CR>", { silent = true })
+vim.keymap.set("n", "<leader>ws", "<cmd>split<CR>", { silent = true })
+vim.keymap.set("n", "<leader>wl", "<cmd>wincmd l<CR>", { silent = true })
+vim.keymap.set("n", "<leader>wh", "<cmd>wincmd h<CR>", { silent = true })
+vim.keymap.set("n", "<leader>wj", "<cmd>wincmd j<CR>", { silent = true })
+vim.keymap.set("n", "<leader>wk", "<cmd>wincmd k<CR>", { silent = true })
