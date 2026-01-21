f353949e8cd35ed376722be25531ee2251d150f4
Ryan Harris <harrisryan1@gmail.com>
2026-01-21 14:19:09 -0500

    nvim: remove cmp overrides for avante

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -43,20 +43,10 @@ return {
         config = function()
             local cmp = require("cmp")
             cmp.setup {
-                mapping = {
-                    ["<C-n>"] = cmp.mapping.select_next_item(),
-                    ["<C-p>"] = cmp.mapping.select_prev_item(),
-                    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.close(),
-                    ["<CR>"] = cmp.mapping.confirm { select = true },
-                },
                 sources = {
                     { name = "nvim_lsp" },
                     { name = "buffer" },
                     { name = "path" },
-                    { name = "avante" },
                 },
             }
         end
